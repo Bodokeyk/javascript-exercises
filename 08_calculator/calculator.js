f0074a41ee4090ec9cce25bcc6b1fcfb962c6db5
bobodokey <94241624+Bodokeyk@users.noreply.github.com>
2024-06-26 16:22:01 -0600

    completed calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((container, a)=>{
+   return container + a;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+const arrayTo = a;
+return arrayTo.reduce((container, a)=>{
+  return container * a
+},1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+const arraOf = [a]
+if(!a){
+  return 1
+}
+for(let i = a - 1; i > 1  ;i--){
+arraOf[0] = arraOf[0] * i
+}
+ return arraOf[0]
 };
 
 // Do not edit below this line
