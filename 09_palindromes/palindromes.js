ca79361b30babddf7fe72c450f93cac84838ae8c
bobodokey <94241624+Bodokeyk@users.noreply.github.com>
2024-06-26 18:50:03 -0600

    completed fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (stringTo) {
+const arrayOfPalindrome = stringTo.toLowerCase().split("").filter((char) =>
+    {
+        if((char >= "a" && char <= "z")||(char >="A" && char <= "Z")||
+    (char >= "0" && char <= "9")){
+            return char
+        }
+    }
+)
+const arrayPalindromed = arrayOfPalindrome.slice().reverse()
+console.log(arrayOfPalindrome.join("") + " ==  " + arrayPalindromed.join("") )
+if(arrayOfPalindrome.join("") === arrayPalindromed.join("")){
+   return true
+}else{
+   return false
+}
 };
 
 // Do not edit below this line
